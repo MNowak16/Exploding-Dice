ae18d7986b24412dbbcbd3c8dee57398e9e758c2
mnowak16@my.wctc.edu <Cr34t!ve6>
2022-05-04 10:47:41 -0500

    scoring working properly with die in array

--- code.js
+++ code.js
@@ -6,13 +6,20 @@ function rollDice(event) {
     //roll dice
     let die;
     let score = 0;
+    let roll = [];
 
     for (let i = 0; i < 5; i++) {
         die = Math.floor(Math.random() * 6) + 1;
         $(`td#d${i + 1}`).text(die);
 
+        //append die to roll
+        roll.push(die);
+
         //count score
-        if (die !== 2 && die !== 5) {
+        //if 2 or 5 in array of die......
+        if (roll.includes(2) || roll.includes(5)) {
+            score = 0;
+        } else {
             score += die;
         }
 
@@ -24,4 +31,7 @@ function rollDice(event) {
 
     //display score
     $("h2#score").text(`Score: ${score}`);
+
+    //if class != exploded, roll again
+
 }
