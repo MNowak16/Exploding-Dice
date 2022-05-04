9cc8e7378e68e0a1e14e919e60c6f6f307e03b91
mnowak16@my.wctc.edu <Cr34t!ve6>
2022-05-04 09:51:39 -0500

    First roll working

--- code.js
+++ code.js
@@ -3,16 +3,25 @@ $("button#roll").on("click", rollDice);
 function rollDice(event) {
     event.preventDefault();
 
-    //randomize dice roll
+    //roll dice
     let die;
+    let score = 0;
+
     for (let i = 0; i < 5; i++) {
         die = Math.floor(Math.random() * 6) + 1;
         $(`td#d${i + 1}`).text(die);
+
+        //count score
+        if (die !== 2 && die !== 5) {
+            score += die;
+        }
+
+        //explode 2's and 5's
+        if (die === 2 || die === 5) {
+            $(`td#d${i +1}`).addClass("exploded");
+        }
     }
 
-    //explode 2's and 5's
-
-    //count score
-
     //display score
+    $("h2#score").text(`Score: ${score}`);
 }
