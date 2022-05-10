2e26685d6e33b6f734e46c241f5acb082c8a283c
mnowak16@my.wctc.edu <Cr34t!ve6>
2022-05-09 20:14:41 -0500

    current status

--- code2.js
+++ code2.js
@@ -1,38 +1,56 @@
 $("button#roll").on("click", playGame);
 
+/*for(let oneDice of allDice) {
+    //displays face of die
+    console.log(oneDice.face);
+    //displays exploded status
+    console.log(oneDice.exploded);
+}*/
+
+//parallel arrays for die & exploded status
+let roll = [];
+let exploded = [];
+
 let score = 0;
 let totalScore = 0;
 
 function playGame(event) {
     event.preventDefault();
     rollDice();
+
+    /*    while (exploded.includes("false")) {
+            rollDice();
+        }*/
+    //gameOver();
 }
 
 function rollDice() {
-    let die = 0;
-    let roll = [];
-
-    //roll dice
+    //roll dice that do not have exploded class
     for (let i = 0; i < 5; i++) {
+        let die = 0;
+
         die = Math.floor(Math.random() * 6) + 1;
         $(`td#d${i + 1}`).text(die);
 
         //append die to roll
         roll.push(die);
 
-        calcScore(roll);
+        //totalScore = current total score + current roll score
+        totalScore += calcScore(roll);
 
-        //display score
-        $("h2#score").text(`Score: ${score}`);
+        //display the total score
+        $("h2#score").text(`Score: ${totalScore}`);
 
         //explode 2's and 5's
         if (die === 2 || die === 5) {
             $(`td#d${i +1}`).addClass("exploded");
+            exploded.push("true");
+        } else {
+            exploded.push("false");
         }
     }
     return roll;
 }
-
 
 function calcScore(roll) {
     //count score if 2 or 5 in array of die
@@ -44,4 +62,8 @@ function calcScore(roll) {
         }, 0);
     }
     return Number(score);
+}
+
+function gameOver() {
+    $("h2#gameStatus").text("GAME OVER!!");
 }
